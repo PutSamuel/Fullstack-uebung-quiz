f948332775c517a68d6bbfd7ee2af0b60c176918
samuel.puetz <samuel.puetz@accenture.com>
2026-09-16 16:59:23 +0200

    Java Script Übung

--- script.js
+++ script.js
@@ -1,2 +1,82 @@
-console.log("Hello, World!"); 
+function function1() {
+    let result = 5 + 10;
+    let result2 = 5 * 10;
+    let result3 = 5 / 10;       
+}
+console.log(result);
+
+
+
+// function functionName(zahl1, zahl2) {
+//     let result = zahl1 + zahl2;
+//     return result;
+// }   
+// console.log(functionName(5, 10));
+// let result = functionName(3, 8);
+// console.log(result);
+
+
+// console.log(5 == '5');
+// console.log(5 === '5');
+// console.log(5 != '5');
+// console.log(5 <= 5);
+// console.log(5 > 5);
+// console.log("Alpha" < "Beta");
+// console.log("Alpha" < "Beta" && "Beta" < "Gamma");
+// console.log("Alpha" < "Beta" || "Beta" < "Gamma");
+
+
+// let firstName = "Max";
+// let lastName = "Mustermann";
+
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+
+// let zahl1 = 5;
+// let zahl2 = 10;
+
+// console.log(zahl1 + zahl2);
+// console.log(zahl1 - zahl2);
+// console.log(zahl1 * zahl2);
+// console.log(zahl1 / zahl2);
+
+// zahl1++;
+// console.log(zahl1);
+// zahl1 = zahl1 + 1;
+// console.log(zahl1);
+
+// zahl1 = zahl1 + zahl2;
+// zahl1 += zahl2;
+// zahl1 += 5;
+// console.log(zahl1);
+
+
+
+// let zahlWurdeErstellt = 5;
+// let stringWurdeErstellt = "Hello World!"; //String
+// let booleanWurdeErstellt = false;
+
+
+
+// console.log(zahlWurdeErstellt);
+// console.log(stringWurdeErstellt);
+// console.log(booleanWurdeErstellt);
+
+
+// zahlWurdeErstellt = 10;
+// stringWurdeErstellt = "Hallo Sternee!";
+// booleanWurdeErstellt = true;
+
+// console.log(zahlWurdeErstellt);
+// console.log(stringWurdeErstellt);
+// console.log(booleanWurdeErstellt);
+
+
+// let neueVariable = stringWurdeErstellt
+// console.log(neueVariable);
+
+
+
+
 
